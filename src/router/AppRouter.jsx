c731c8fbee15ddcae66c88de6ee3e7f69ce2aa69
Tomas Sigel <tomasmsigel@hotmail.com>
2023-09-10 11:16:18 -0300

    logeados solo para /cart /checkout y miscompras

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -7,11 +7,14 @@ import ForgotPassword from "../components/pages/forgotPassword/ForgotPassword";
 import Dashboard from "../components/pages/dashboard/Dashboard";
 import ProtectedAdmin from "./ProtectedAdmin";
 import ProtectedUsers from "./ProtectedUsers";
+import ItemListContainer from "../components/pages/itemlist/ItemListContainer"; //
+import Home from "../components/pages/home/Home"; //
+import ItemDetailContainer from "../components/pages/itemDetail/ItemDetailContainer"; //
 
 const AppRouter = () => {
   return (
     <Routes>
-      {/* VISTA PARA LOS USUARIOS LOGEADOS */}
+      {/* VISTA PARA LOS USUARIOS LOGEADOS, solamente /cart, /checkout y miscompras */}
       <Route element={<ProtectedUsers />}>
         <Route element={<Navbar />}>
           {routes.map(({ id, path, Element }) => (
@@ -27,6 +30,7 @@ const AppRouter = () => {
         </Route>
       </Route>
 
+      {/* VISTA PARA TODOS LOS USUARIOS LOGEADOS O NO */}
       {/* Login */}
       <Route path="/login" element={<Login />} />
 
@@ -35,6 +39,16 @@ const AppRouter = () => {
 
       {/* forgot password  */}
       <Route path="/forgot-password" element={<ForgotPassword />} />
+
+      {/* rutas que antes estaban bloqueadas con el login pero nadie se registra antes de mirar */}
+      <Route element={<Navbar />}>
+        {/* home */}
+        <Route path="/" element={<Home />} />
+        {/* tienda */}
+        <Route path="/shop" element={<ItemListContainer />} />
+        {/* detalle del producto */}
+        <Route path="/itemDetail/:id" element={<ItemDetailContainer />} />
+      </Route>
 
       <Route path="*" element={<h1>Not found</h1>} />
     </Routes>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,26 +1,26 @@
 import Cart from "../components/pages/cart/Cart";
 import Checkout from "../components/pages/checkout/Checkout";
-import Home from "../components/pages/home/Home";
-import ItemDetail from "../components/pages/itemDetail/ItemDetailContainer";
-import ItemListContainer from "../components/pages/itemlist/ItemListContainer";
+//import Home from "../components/pages/home/Home";
+//import ItemDetail from "../components/pages/itemDetail/ItemDetailContainer";
+//import ItemListContainer from "../components/pages/itemlist/ItemListContainer";
 import UserOrders from "../components/pages/userOrders/UserOrders";
 
 export const routes = [
-  {
+  /*   {
     id: "home",
     path: "/",
     Element: Home,
-  },
-  {
+  }, */
+  /*   {
     id: "shop",
     path: "/shop",
     Element: ItemListContainer,
-  },
-  {
+  }, */
+  /*   {
     id: "detail",
     path: "/itemDetail/:id",
     Element: ItemDetail,
-  },
+  }, */
   {
     id: "cart",
     path: "/cart",
@@ -36,5 +36,4 @@ export const routes = [
     path: "/user-orders",
     Element: UserOrders,
   },
-  //
 ];
